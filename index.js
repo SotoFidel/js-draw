72055fbb0bffee2cf109c8f8ea3b0846a8262e70
Fidel Soto <fidelsotostem@gmail.com>
2025-09-18 23:59:08 -0600

    added cursor icons to show where the user is going to drow and how big the draw/erase will be

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // const canvas = document.getElementById("canvas");
 let canvas;
+let toolLayer;
 const canvasContainer = document.getElementById("canvasContainer");
 let ctx;
+let tctx;
 
 let cursorEnabled = false;
 let offsetX = 0;
@@ -35,7 +37,11 @@ function setupCanvasEvents() {
                 ctx.stroke();
             }
             if (currentMode.mode == "Erasing") {
-                ctx.clearRect(Math.max(newCoords.x - (currentMode.strokeWidth / 2), 0), Math.max(newCoords.y - (currentMode.strokeWidth / 2), 0), currentMode.strokeWidth, currentMode.strokeWidth);
+                ctx.clearRect(
+                    Math.max(newCoords.x - (currentMode.strokeWidth / 2), 0),
+                    Math.max(newCoords.y - (currentMode.strokeWidth / 2), 0),
+                    currentMode.strokeWidth,
+                    currentMode.strokeWidth);
             }
         }
     });
@@ -44,9 +50,11 @@ function setupCanvasEvents() {
     ["mouseup", "mouseleave"].forEach((eventType) => {
         canvas.addEventListener(eventType, () => {
             cursorEnabled = false;
+            tctx.clearRect(0, 0, toolLayer.width, toolLayer.height);
             changeStack.push(ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height));
         });
     });
+
 
     canvas.addEventListener("mousemove", (event) => {
         oldCoords = newCoords;
@@ -104,16 +112,41 @@ function setupUiEvents() {
     let brushSizeInput = document.querySelector("#ribbon > input#size");
     let brushSizeOutput = document.querySelector("#sizeVal");
     brushSizeOutput.value = brushSizeInput.value;
+
     brushSizeInput.addEventListener("input", (event) => {
         brushSizeOutput.value = event.target.value;
         modes[currentMode.mode].strokeWidth = event.target.value;
     });
+
+    brushSizeInput.addEventListener("wheel", (event) => {
+        if (event.deltaY < 0) {
+            brushSizeInput.stepUp(2);
+        }
+        else {
+            brushSizeInput.stepDown(2);
+        }
+        brushSizeOutput.value = brushSizeInput.value;
+        modes[currentMode.mode].strokeWidth = brushSizeInput.value;
+    });
+
     brushSizeOutput.addEventListener("change", (event) => {
         if (event.target.value > 500) {
             event.target.value = 500;
         }
         brushSizeInput.value = event.target.value;
         modes[currentMode.mode].strokeWidth = event.target.value;
+    });
+
+
+    brushSizeOutput.addEventListener("wheel", (event) => {
+        if (event.deltaY < 0) {
+            brushSizeOutput.stepUp(2);
+        }
+        else {
+            brushSizeOutput.stepDown(2);
+        }
+        brushSizeInput.value = brushSizeOutput.value;
+        modes[currentMode.mode].strokeWidth = brushSizeOutput.value;
     });
 
     document.querySelector("#colorPicker").addEventListener("change", (event) => {
@@ -127,6 +160,13 @@ function update() {
 }
 
 function draw() {
+
+    tctx.clearRect(0, 0, toolLayer.width, toolLayer.height);
+    tctx.beginPath();
+    // tctx.rect(newCoords.x, newCoords.y, currentMode.strokeWidth, currentMode.strokeWidth);
+    tctx.arc(Math.max(newCoords.x, 0),
+        Math.max(newCoords.y, 0), currentMode.strokeWidth, 0, 2 * Math.PI);
+    tctx.stroke();
 
     if (cursorEnabled) {
         for (let i = 0; i <= 1; i += .01) {
@@ -143,6 +183,14 @@ function draw() {
 }
 
 function erase() {
+    tctx.clearRect(0, 0, toolLayer.width, toolLayer.height);
+    tctx.beginPath();
+    tctx.rect(
+        Math.max(newCoords.x - (currentMode.strokeWidth / 2), 0),
+        Math.max(newCoords.y - (currentMode.strokeWidth / 2), 0),
+        currentMode.strokeWidth,
+        currentMode.strokeWidth);
+    tctx.stroke();
     if (cursorEnabled) {
         let nc;
         // console.log("erase ", oldCoords, newCoords);
@@ -162,10 +210,33 @@ function interpolate(a, b, t) // points A and B, frac between 0 and 1
 }
 
 function setMethod(method) {
+    tctx.clearRect(0, 0, toolLayer.width, toolLayer.height);
     modes[currentMode.mode].strokeWidth = currentMode.strokeWidth;
     currentMode = modes[method];
     document.querySelector("#size").value = currentMode.strokeWidth;
     document.querySelector("#sizeVal").value = currentMode.strokeWidth;
+    switch (method) {
+        case "Drawing":
+            document.querySelector('.tool:has(#erase-btn)').classList.remove('chosen');
+            document.querySelector('.tool:has(#draw-btn)').classList.add('chosen');
+            break;
+        case "Erasing":
+            document.querySelector('.tool:has(#draw-btn)').classList.remove('chosen');
+            document.querySelector('.tool:has(#erase-btn)').classList.add('chosen');
+            break;
+        default:
+            break;
+    }
+}
+
+function exportImage() {
+    let tempAnchor = document.createElement('a');
+    let imageUrl = canvas.toDataURL("image/png");
+    tempAnchor.href = imageUrl;
+    tempAnchor.download = "exportedImage";
+    document.querySelector('#ribbon').appendChild(tempAnchor);
+    tempAnchor.click();
+    document.querySelector('#ribbon').removeChild(tempAnchor);
 }
 
 function calculateOffsets() {
@@ -175,14 +246,27 @@ function calculateOffsets() {
 
 function createCanvas() {
     let canvasContainerRect = canvasContainer.getBoundingClientRect();
+
     canvas = document.createElement("canvas");
     canvas.setAttribute("id", "canvas");
     canvas.setAttribute("width", `${canvasContainerRect.width}`);
     canvas.setAttribute("height", `${canvasContainerRect.height}`);
+
+    toolLayer = document.createElement("canvas");
+    toolLayer.setAttribute("id", "toolLayer");
+    toolLayer.setAttribute("width", `${canvasContainerRect.width}`);
+    toolLayer.setAttribute("height", `${canvasContainerRect.height}`);
+
     canvasContainer.replaceWith(canvas);
+    canvas.after(toolLayer);
     ctx = canvas.getContext("2d", { willReadFrequently: true });
     ctx.lineWidth = 0;
     ctx.fillStyle = "black";
+
+    tctx = toolLayer.getContext("2d", { willReadFrequently: true });
+    tctx.lineWidth = 0;
+    tctx.fillStyle = "black";
+
     changeStack.push(ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height));
 }
 
