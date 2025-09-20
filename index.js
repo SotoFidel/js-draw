1b3131d54735ed4ed482826745c6a6ec1ebd89e5
Fidel Soto <fidelsotostem@gmail.com>
2025-09-19 18:05:10 -0600

    Reorganized functions

--- index.js
+++ index.js
@@ -22,6 +22,142 @@ let modes = {
 
 let currentMode = modes.Drawing;
 
+function clearCanvas() {
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function draw() {
+
+    toolContext.clearRect(0, 0, toolCanvas.width, toolCanvas.height);
+    toolContext.beginPath();
+    // tctx.rect(newCoords.x, newCoords.y, currentMode.strokeWidth, currentMode.strokeWidth);
+    toolContext.arc(Math.max(newCoords.x, 0),
+        Math.max(newCoords.y, 0), currentMode.strokeWidth, 0, 2 * Math.PI);
+    toolContext.stroke();
+
+    if (isClicking) {
+        for (let i = 0; i <= 1; i += .01) {
+            nc = interpolate(oldCoords, newCoords, i);
+            canvasContext.beginPath();
+            canvasContext.arc(Math.max(nc.x, 0),
+                Math.max(nc.y, 0), currentMode.strokeWidth, 0, 2 * Math.PI);
+            canvasContext.strokeStyle = color;
+            canvasContext.fillStyle = color;
+            canvasContext.fill();
+            canvasContext.stroke();
+        }
+    }
+}
+
+function erase() {
+    toolContext.clearRect(0, 0, toolCanvas.width, toolCanvas.height);
+    toolContext.beginPath();
+    toolContext.rect(
+        Math.max(newCoords.x - (currentMode.strokeWidth / 2), 0),
+        Math.max(newCoords.y - (currentMode.strokeWidth / 2), 0),
+        currentMode.strokeWidth,
+        currentMode.strokeWidth);
+    toolContext.stroke();
+    if (isClicking) {
+        let nc;
+        // console.log("erase ", oldCoords, newCoords);
+        for (let i = 0; i <= 1; i += .05) {
+            nc = interpolate(oldCoords, newCoords, i);
+            canvasContext.clearRect(Math.max(nc.x - (currentMode.strokeWidth / 2), 0), Math.max(nc.y - (currentMode.strokeWidth / 2), 0), currentMode.strokeWidth, currentMode.strokeWidth);
+        }
+        // ctx.clearRect(Math.max(newCoords.x - 5, 0), Math.max(newCoords.y - 5, 0), 10, 10);
+    }
+}
+
+function exportImage() {
+    let tempAnchor = document.createElement('a');
+    let imageUrl = canvas.toDataURL("image/png");
+    tempAnchor.href = imageUrl;
+    tempAnchor.download = "exportedImage";
+    document.querySelector('#ribbon').appendChild(tempAnchor);
+    tempAnchor.click();
+    document.querySelector('#ribbon').removeChild(tempAnchor);
+}
+
+function bucketFill() {
+
+}
+
+// points A and B, frac between 0 and 1
+function interpolate(a, b, t) {
+    var nx = a.x + (b.x - a.x) * t;
+    var ny = a.y + (b.y - a.y) * t;
+    return { x: nx, y: ny };
+}
+
+function redoAction() {
+    if (redoStack.length > 0) {
+        canvasContext.putImageData(redoStack.pop(), 0, 0);
+    }
+}
+
+function undoAction() {
+    if (changeStack.length > 1) {
+        redoStack.push(changeStack.pop());
+        canvasContext.putImageData(changeStack[changeStack.length - 1], 0, 0);
+    } else if (changeStack.length == 1) {
+        canvasContext.putImageData(changeStack[0], 0, 0);
+        redoStack.push(changeStack.pop());
+    }
+}
+
+function setMethod(method) {
+    toolContext.clearRect(0, 0, toolCanvas.width, toolCanvas.height);
+    modes[currentMode.mode].strokeWidth = currentMode.strokeWidth;
+    currentMode = modes[method];
+    document.querySelector("#size").value = currentMode.strokeWidth;
+    document.querySelector("#sizeVal").value = currentMode.strokeWidth;
+    switch (method) {
+        case "Drawing":
+            document.querySelector('.tool:has(#erase-btn)').classList.remove('chosen');
+            document.querySelector('.tool:has(#draw-btn)').classList.add('chosen');
+            break;
+        case "Erasing":
+            document.querySelector('.tool:has(#draw-btn)').classList.remove('chosen');
+            document.querySelector('.tool:has(#erase-btn)').classList.add('chosen');
+            break;
+        default:
+            break;
+    }
+}
+
+
+function calculateOffsets() {
+    canvasOffsetX = canvas.getBoundingClientRect().x;
+    canvasOffsetY = canvas.getBoundingClientRect().y;
+}
+
+function createCanvas() {
+    let canvasContainerRect = canvasContainer.getBoundingClientRect();
+
+    canvas = document.createElement("canvas");
+    canvas.setAttribute("id", "canvas");
+    canvas.setAttribute("width", `${canvasContainerRect.width}`);
+    canvas.setAttribute("height", `${canvasContainerRect.height}`);
+
+    toolCanvas = document.createElement("canvas");
+    toolCanvas.setAttribute("id", "toolLayer");
+    toolCanvas.setAttribute("width", `${canvasContainerRect.width}`);
+    toolCanvas.setAttribute("height", `${canvasContainerRect.height}`);
+
+    canvasContainer.replaceWith(canvas);
+    canvas.after(toolCanvas);
+    canvasContext = canvas.getContext("2d", { willReadFrequently: true });
+    canvasContext.lineWidth = 0;
+    canvasContext.fillStyle = "black";
+
+    toolContext = toolCanvas.getContext("2d", { willReadFrequently: true });
+    toolContext.lineWidth = 0;
+    toolContext.fillStyle = "black";
+
+    changeStack.push(canvasContext.getImageData(0, 0, canvasContext.canvas.width, canvasContext.canvas.height));
+}
+
 function setupCanvasEvents() {
     canvas.addEventListener("mousedown", (event) => {
         // left click to draw
@@ -29,20 +165,6 @@ function setupCanvasEvents() {
             redoStack = [];
             isClicking = true;
             currentMode.fn();
-            // if (currentMode.mode == "Drawing") {
-            //     canvasContext.beginPath();
-            //     canvasContext.arc(Math.max(newCoords.x, 0),
-            //         Math.max(newCoords.y, 0), currentMode.strokeWidth, 0, 2 * Math.PI);
-            //     canvasContext.fill();
-            //     canvasContext.stroke();
-            // }
-            // if (currentMode.mode == "Erasing") {
-            //     canvasContext.clearRect(
-            //         Math.max(newCoords.x - (currentMode.strokeWidth / 2), 0),
-            //         Math.max(newCoords.y - (currentMode.strokeWidth / 2), 0),
-            //         currentMode.strokeWidth,
-            //         currentMode.strokeWidth);
-            // }
         }
     });
 
@@ -50,8 +172,10 @@ function setupCanvasEvents() {
     ["mouseup", "mouseleave"].forEach((eventType) => {
         canvas.addEventListener(eventType, () => {
             isClicking = false;
-            toolContext.clearRect(0, 0, toolCanvas.width, toolCanvas.height);
             changeStack.push(canvasContext.getImageData(0, 0, canvasContext.canvas.width, canvasContext.canvas.height));
+            if (eventType == "mouseleave") {
+                toolContext.clearRect(0, 0, toolCanvas.width, toolCanvas.height);
+            }
         });
     });
 
@@ -60,29 +184,6 @@ function setupCanvasEvents() {
         oldCoords = newCoords;
         newCoords = { x: event.x - canvasOffsetX, y: event.y - canvasOffsetY };
 
-        // console.log("Painting new blue at: ", newCoords);
-        // ctx.beginPath();
-        // ctx.strokeStyle = "blue";
-        // ctx.lineWidth = 3;
-        // ctx.rect(newCoords.x - 5, newCoords.y - 5, 10, 10);
-        // ctx.stroke();
-        // console.log("painting old red at: ", oldCoords);
-        // ctx.beginPath();
-        // ctx.strokeStyle = "red";
-        // ctx.lineWidth = 2;
-        // ctx.rect(oldCoords.x - 5, oldCoords.y - 5, 10, 10);
-        // ctx.stroke();
-        // let newC;
-        //
-        // ctx.beginPath();
-        // ctx.strokeStyle = "purple";
-        // ctx.lineWidth = 4;
-        // for (let i = 0; i <= 1; i += .05) {
-        //     newC = interpolate(oldCoords, newCoords, i);
-        //     ctx.rect(newC.x - 5, newC.y - 5, 10, 10);
-        //     ctx.stroke();
-        // }
-        // console.log("mousemove ", oldCoords, newCoords);
         currentMode.fn();
     });
 
@@ -96,8 +197,6 @@ function setupCanvasEvents() {
         }
 
     });
-
-
 }
 
 function setupUiEvents() {
@@ -148,147 +247,10 @@ function setupUiEvents() {
 
 }
 
-function update() {
-    currentMode.fn();
-    requestAnimationFrame(update);
-}
-
-function draw() {
-
-    toolContext.clearRect(0, 0, toolCanvas.width, toolCanvas.height);
-    toolContext.beginPath();
-    // tctx.rect(newCoords.x, newCoords.y, currentMode.strokeWidth, currentMode.strokeWidth);
-    toolContext.arc(Math.max(newCoords.x, 0),
-        Math.max(newCoords.y, 0), currentMode.strokeWidth, 0, 2 * Math.PI);
-    toolContext.stroke();
-
-    if (isClicking) {
-        for (let i = 0; i <= 1; i += .01) {
-            nc = interpolate(oldCoords, newCoords, i);
-            canvasContext.beginPath();
-            canvasContext.arc(Math.max(nc.x, 0),
-                Math.max(nc.y, 0), currentMode.strokeWidth, 0, 2 * Math.PI);
-            canvasContext.strokeStyle = color;
-            canvasContext.fillStyle = color;
-            canvasContext.fill();
-            canvasContext.stroke();
-        }
-    }
-}
-
-function erase() {
-    toolContext.clearRect(0, 0, toolCanvas.width, toolCanvas.height);
-    toolContext.beginPath();
-    toolContext.rect(
-        Math.max(newCoords.x - (currentMode.strokeWidth / 2), 0),
-        Math.max(newCoords.y - (currentMode.strokeWidth / 2), 0),
-        currentMode.strokeWidth,
-        currentMode.strokeWidth);
-    toolContext.stroke();
-    if (isClicking) {
-        let nc;
-        // console.log("erase ", oldCoords, newCoords);
-        for (let i = 0; i <= 1; i += .05) {
-            nc = interpolate(oldCoords, newCoords, i);
-            canvasContext.clearRect(Math.max(nc.x - (currentMode.strokeWidth / 2), 0), Math.max(nc.y - (currentMode.strokeWidth / 2), 0), currentMode.strokeWidth, currentMode.strokeWidth);
-        }
-        // ctx.clearRect(Math.max(newCoords.x - 5, 0), Math.max(newCoords.y - 5, 0), 10, 10);
-    }
-}
-
-function undoAction() {
-    if (changeStack.length > 1) {
-        redoStack.push(changeStack.pop());
-        canvasContext.putImageData(changeStack[changeStack.length - 1], 0, 0);
-    } else if (changeStack.length == 1) {
-        canvasContext.putImageData(changeStack[0], 0, 0);
-        redoStack.push(changeStack.pop());
-    }
-}
-
-function redoAction() {
-    if (redoStack.length > 0) {
-        canvasContext.putImageData(redoStack.pop(), 0, 0);
-    }
-}
-
-function interpolate(a, b, t) // points A and B, frac between 0 and 1
-{
-    var nx = a.x + (b.x - a.x) * t;
-    var ny = a.y + (b.y - a.y) * t;
-    return { x: nx, y: ny };
-}
-
-function setMethod(method) {
-    toolContext.clearRect(0, 0, toolCanvas.width, toolCanvas.height);
-    modes[currentMode.mode].strokeWidth = currentMode.strokeWidth;
-    currentMode = modes[method];
-    document.querySelector("#size").value = currentMode.strokeWidth;
-    document.querySelector("#sizeVal").value = currentMode.strokeWidth;
-    switch (method) {
-        case "Drawing":
-            document.querySelector('.tool:has(#erase-btn)').classList.remove('chosen');
-            document.querySelector('.tool:has(#draw-btn)').classList.add('chosen');
-            break;
-        case "Erasing":
-            document.querySelector('.tool:has(#draw-btn)').classList.remove('chosen');
-            document.querySelector('.tool:has(#erase-btn)').classList.add('chosen');
-            break;
-        default:
-            break;
-    }
-}
-
-function clearCanvas() {
-    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-function exportImage() {
-    let tempAnchor = document.createElement('a');
-    let imageUrl = canvas.toDataURL("image/png");
-    tempAnchor.href = imageUrl;
-    tempAnchor.download = "exportedImage";
-    document.querySelector('#ribbon').appendChild(tempAnchor);
-    tempAnchor.click();
-    document.querySelector('#ribbon').removeChild(tempAnchor);
-}
-
-function calculateOffsets() {
-    canvasOffsetX = canvas.getBoundingClientRect().x;
-    canvasOffsetY = canvas.getBoundingClientRect().y;
-}
-
-function createCanvas() {
-    let canvasContainerRect = canvasContainer.getBoundingClientRect();
-
-    canvas = document.createElement("canvas");
-    canvas.setAttribute("id", "canvas");
-    canvas.setAttribute("width", `${canvasContainerRect.width}`);
-    canvas.setAttribute("height", `${canvasContainerRect.height}`);
-
-    toolCanvas = document.createElement("canvas");
-    toolCanvas.setAttribute("id", "toolLayer");
-    toolCanvas.setAttribute("width", `${canvasContainerRect.width}`);
-    toolCanvas.setAttribute("height", `${canvasContainerRect.height}`);
-
-    canvasContainer.replaceWith(canvas);
-    canvas.after(toolCanvas);
-    canvasContext = canvas.getContext("2d", { willReadFrequently: true });
-    canvasContext.lineWidth = 0;
-    canvasContext.fillStyle = "black";
-
-    toolContext = toolCanvas.getContext("2d", { willReadFrequently: true });
-    toolContext.lineWidth = 0;
-    toolContext.fillStyle = "black";
-
-    changeStack.push(canvasContext.getImageData(0, 0, canvasContext.canvas.width, canvasContext.canvas.height));
-}
-
 window.onload = function() {
     createCanvas();
     calculateOffsets();
     setupCanvasEvents();
     setupUiEvents();
-    // requestAnimationFrame(update);
 }
 
