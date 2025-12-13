e13ce4fb2811f1ef3b9c6e98afd9a99b0bafd479
Fidel Soto <fidelsotostem@gmail.com>
2025-12-13 14:21:22 -0700

    Added signalR server and setup frontend to connect to it

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1527,6 +1527,45 @@ function setupUiEvents() {
   });
 }
 
+function setupSignalRTest() {
+  document.querySelector("#dialog").toggleAttribute("open");
+  var connection = new signalR.HubConnectionBuilder()
+    .withUrl("https://localhost:7196/chatHub")
+    .build();
+
+  //Disable the send button until connection is established.
+  document.getElementById("sendButton").disabled = true;
+
+  connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you
+    // should be aware of possible script injection concerns.
+    li.textContent = `${user} says ${message}`;
+  });
+
+  connection
+    .start()
+    .then(function () {
+      document.getElementById("sendButton").disabled = false;
+    })
+    .catch(function (err) {
+      return console.error(err.toString());
+    });
+
+  document
+    .getElementById("sendButton")
+    .addEventListener("click", function (event) {
+      var user = document.getElementById("userInput").value;
+      var message = document.getElementById("messageInput").value;
+      connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+      });
+      event.preventDefault();
+    });
+}
+
 window.onload = function () {
   createCanvas();
   calculateOffsets();
